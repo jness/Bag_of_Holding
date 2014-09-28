8b908b06fd894aa43eafcd97bb473ed429ea4063
Jeffrey Ness <jeffrey.ness@rackspace.com>
2014-09-28 09:47:20 -0500

    fix temps and add admin views

--- hub/admin.py
+++ hub/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from hub.models import Icon, Item, Character
 
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brewery', 'author']
+admin.site.register(Item, ItemAdmin)
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brewery', 'author']
+admin.site.register(Character, CharacterAdmin)
+
 admin.site.register(Icon)
-admin.site.register(Item)
-admin.site.register(Character)
