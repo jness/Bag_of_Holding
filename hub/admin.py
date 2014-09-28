b5a6eda9df6dd5f7052503627053b8bef027173f
Jeffrey Ness <jeffrey.ness@rackspace.com>
2014-09-28 09:48:47 -0500

    correct names

--- hub/admin.py
+++ hub/admin.py
@@ -3,12 +3,12 @@ from hub.models import Icon, Item, Character
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['name', 'brewery', 'author']
+    list_display = ['name', 'character']
 admin.site.register(Item, ItemAdmin)
 
 
 class CharacterAdmin(admin.ModelAdmin):
-    list_display = ['name', 'brewery', 'author']
+    list_display = ['name', 'owner']
 admin.site.register(Character, CharacterAdmin)
 
 admin.site.register(Icon)
